99c4a21371ec0fa8ea9cca89bf47f68b1e5c6920
Jiro Delfino <jiro.delfino24@gmail.com>
2025-04-15 17:12:43 +0800

    App: Fix container width and height to cover full screen. See Task 4 on docs for more info.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { LoginForm } from './components/login-form'
 
 function App() {
   return (
-    <div className="flex min-h-auto flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+      <div className="box-border flex min-h-screen w-full flex-col items-center justify-center bg-muted p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-3xl">
+          <LoginForm />
+        </div>
       </div>
-    </div>
   )
-}
+} 
 
 export default App
